536ee30a4647bece5d261b18616e89811c756060
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-11-27 22:30:29 +0100

    Refactored coders, decoders, exceptions.

---
+++ Lzma/Helpers/InvalidDataException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lzma
+{
+    [Serializable]
+    public class InvalidDataException : ArgumentException
+    {
+        public InvalidDataException() : base("Invalid parameter.") { }
+    }
+}
---
+++ Lzma/Helpers/InvalidParameterException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lzma
+{
+    [Serializable]
+    public class InvalidParameterException : ArgumentException
+    {
+        public InvalidParameterException() : base("Invalid parameter.") { }
+    }
+}
