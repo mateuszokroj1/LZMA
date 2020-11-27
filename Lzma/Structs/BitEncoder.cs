2baaa72c5b1facdbcd2e821a0f98754e6e74cfe9
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-11-27 20:58:39 +0100

    Refactoring of RangeCoder directory. Refreshed code style. Removed comments and unrequired methods.

---
+++ Lzma/Structs/BitEncoder.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace Lzma.Structs
+{
+    internal struct BitEncoder
+    {
+        #region Constructor
+
+        static BitEncoder()
+        {
+            const int kNumBits = kNumBitModelTotalBits - kNumMoveReducingBits;
+
+            for (int i = kNumBits - 1; i >= 0; i--)
+            {
+                uint start = (uint) 1 << (kNumBits - i - 1);
+                uint end = (uint) 1 << (kNumBits - i);
+
+                for (uint j = start; j < end; j++)
+                    ProbPrices[j] = ((uint)i << kNumBitPriceShiftBits) +
+                        (((end - j) << kNumBitPriceShiftBits) >> (kNumBits - i - 1));
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        public const int kNumBitModelTotalBits = 11;
+        public const uint kBitModelTotal = 1 << kNumBitModelTotalBits;
+        const int kNumMoveBits = 5;
+        const int kNumMoveReducingBits = 2;
+        public const int kNumBitPriceShiftBits = 6;
+
+        private uint Prob;
+
+        private static readonly uint[] ProbPrices = new uint[kBitModelTotal >> kNumMoveReducingBits];
+
+        #endregion
+
+        #region Properties
+
+        public uint Price0 => ProbPrices[Prob >> kNumMoveReducingBits];
+        public uint Price1 => ProbPrices[(kBitModelTotal - Prob) >> kNumMoveReducingBits];
+
+        #endregion
+
+        public void Init() => Prob = kBitModelTotal >> 1;
+
+        public void UpdateModel(uint symbol)
+        {
+            if (symbol == 0)
+                Prob += (kBitModelTotal - Prob) >> kNumMoveBits;
+            else
+                Prob -= (Prob) >> kNumMoveBits;
+        }
+
+        public void Encode(Encoder encoder, uint symbol)
+        {
+            uint newBound = (encoder.Range >> kNumBitModelTotalBits) * Prob;
+
+            if (symbol == 0)
+            {
+                encoder.Range = newBound;
+                Prob += (kBitModelTotal - Prob) >> kNumMoveBits;
+            }
+            else
+            {
+                encoder.Low += newBound;
+                encoder.Range -= newBound;
+                Prob -= (Prob) >> kNumMoveBits;
+            }
+
+            if (encoder.Range < Encoder.kTopValue)
+            {
+                encoder.Range <<= 8;
+                encoder.ShiftLow();
+            }
+        }
+
+        public uint GetPrice(uint symbol) =>
+            ProbPrices[(((Prob - symbol) ^ (-(int)symbol)) & (kBitModelTotal - 1)) >> kNumMoveReducingBits];
+    }
+}
---
+++ Lzma/Structs/BitTreeDecoder.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace Lzma.Structs
+{
+    internal struct BitTreeDecoder
+    {
+        #region Constructor
+
+        public BitTreeDecoder(int numBitLevels)
+        {
+            this.numBitLevels = numBitLevels;
+            models = new BitDecoder[1 << numBitLevels];
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly BitDecoder[] models;
+        private readonly int numBitLevels;
+
+        #endregion
+
+        public void Init()
+        {
+            for (uint i = 1; i < 1 << numBitLevels; i++)
+                models[i].Init();
+        }
+
+        public uint Decode(Decoder rangeDecoder)
+        {
+            uint m = 1;
+
+            for (int bitIndex = numBitLevels; bitIndex > 0; bitIndex--)
+                m = (m << 1) + models[m].Decode(rangeDecoder);
+
+            return m - ((uint)1 << numBitLevels);
+        }
+
+        public uint ReverseDecode(Decoder rangeDecoder)
+        {
+            uint m = 1;
+            uint symbol = 0;
+
+            for (int bitIndex = 0; bitIndex < numBitLevels; bitIndex++)
+            {
+                uint bit = models[m].Decode(rangeDecoder);
+                m <<= 1;
+                m += bit;
+                symbol |= bit << bitIndex;
+            }
+
+            return symbol;
+        }
+
+        public static uint ReverseDecode(BitDecoder[] models, uint startIndex,
+            Decoder rangeDecoder, int numBitLevels)
+        {
+            uint m = 1;
+            uint symbol = 0;
+
+            for (int bitIndex = 0; bitIndex < numBitLevels; bitIndex++)
+            {
+                uint bit = models[startIndex + m].Decode(rangeDecoder);
+                m <<= 1;
+                m += bit;
+                symbol |= (bit << bitIndex);
+            }
+
+            return symbol;
+        }
+    }
+}
